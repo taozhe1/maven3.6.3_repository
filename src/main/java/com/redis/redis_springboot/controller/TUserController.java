37244f10b53ca0d6ffc371ba0c4eb1c42368d318
taozhe <13035637305@wo.cn>
2024-03-27 09:08:49 +0800

    注释

--- src/main/java/com/redis/redis_springboot/controller/TUserController.java
+++ src/main/java/com/redis/redis_springboot/controller/TUserController.java
@@ -28,7 +28,7 @@ public class TUserController {
 
         System.out.println("12313123");
 
-        System.out.println("3333333331");
+        //System.out.println("3333333331");
 
         model.addAttribute("users",tUserService.list());
         return "userList";
